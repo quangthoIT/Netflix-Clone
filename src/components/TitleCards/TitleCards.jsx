eda59e20bfbc723d5bf8f67057800f7a2f3878db
quangthoit04 <quangthoit04@gmail.com>
2025-07-02 00:49:40 +0700

    Banner

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import "./TitleCards.css"
+import React from "react";
+import "./TitleCards.css";
 
 const TitleCards = () => {
-  return (
-    <div className='titlecards'>
-      
-    </div>
-  )
-}
 
-export default TitleCards
+
+  return (
+    <div className='title-cards'>
+
+    </div>
+  );
+};
+
+export default TitleCards;
