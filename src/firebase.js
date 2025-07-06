bc18c075643b9f330a5212fde2da7421f0cafb0a
quangthoit04 <quangthoit04@gmail.com>
2025-07-06 23:24:01 +0700

    Player, Firebase, LoginLogout

---
+++ src/firebase.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAATC5GXOjiZVXlqt7iKJ5GFtHCe4W4oYg",
+  authDomain: "netflix-clone-f8c7a.firebaseapp.com",
+  projectId: "netflix-clone-f8c7a",
+  storageBucket: "netflix-clone-f8c7a.firebasestorage.app",
+  messagingSenderId: "826707911282",
+  appId: "1:826707911282:web:ed5bfd455f3c57c51fd8eb",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+      
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+};
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, db, login, signup, logout };
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
-import "./Player.css"
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDE1MzFhMzJkYzE2OWFiMTg0NjFlMWZiYTFiMWYxMiIsIm5iZiI6MTc1MTczOTM2OS4yNjEsInN1YiI6IjY4Njk2YmU5ZjNmZTNjZjVjYjZlN2IzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sgR3n4h-Nwx5qO993v1aO4_0rtjxuxWqvMgJUQajNjU",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options,
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className='player'>
+      <img
+        src={back_arrow_icon}
+        alt=''
+        onClick={() => {
+          navigate(-1);
+        }}
+      />
       
+      <iframe
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        width='90%'
+        height='90%'
+        title='trailer'
+        frameborder='0'
+        allowFullScreen
+      ></iframe>
+      <div className='player-info'>
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
